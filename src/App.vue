0835191580796e3227096409a98369d3ade247ef
0racularGh0st <nigel4tariang@gmail.com>
2020-10-02 14:13:55 +0530

    Make background fixed

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,33 @@
 <template>
   <div id="main-app">
-    <background></background>
+    <background class="the-background"></background>
+    <home class="home-class"></home>
   </div>
 </template>
 <script>
 import Background from './components/Background';
+import Home from './views/Home';
 export default {
   components:{
-    'background': Background
+    'background': Background,
+    'home': Home
   }
 }
 </script>
-<style lang="scss">
-
+<style scoped lang="scss">
+.the-background{
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    position: fixed;
+    background-size: cover;
+    background-attachment: fixed;
+    background: inherit;
+}
+.home-class{
+    position: relative;
+    color: white;
+    height: 150vh;
+}
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    
+    <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo quidem dolor fugiat deleniti doloremque, quis rem tenetur fugit tempora ea placeat, ab quaerat possimus quia veniam nihil recusandae veritatis, dolore eaque. Minus consectetur repellendus nesciunt magni amet reiciendis sequi impedit doloremque consequuntur officiis. Aperiam fugiat modi eum laboriosam iusto id quis asperiores deserunt tempore sed sint illum mollitia earum minus reiciendis fuga est perspiciatis, ullam voluptates rem? Iusto saepe tempora asperiores ducimus animi tenetur ipsa ad dolorum amet? Quas consequuntur quidem aperiam ut veritatis nemo? Laboriosam porro est quibusdam id nulla ea soluta. Voluptatum voluptas a laboriosam perspiciatis velit natus!</h1>
   </div>
 </template>
 
