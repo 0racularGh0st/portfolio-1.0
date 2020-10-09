942dfd5aa2aa440cdd855f8580317ea0527b2b63
0racularGh0st <nigel4tariang@gmail.com>
2020-10-09 22:07:34 +0530

    Make cursor blink

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <h2 class="content">
-      <div class="intro">
+      <div class="line anim-typewriter">
         Hi, I'm Nigel. FullStack Developer.
       </div>
       
@@ -20,31 +20,41 @@ export default {
 }
 .content{
   margin-top: 50vh;
-  margin-left: 1rem;
-  margin-right: 1rem;
   text-align: center;
   display: flex;
   justify-content: center;
-  font-size: large;
+  font-size: 100%;
 }
-.intro{
+.line{
   color:#94a7b0;
-    animation: type 3s steps(35);
+    animation: type 3s steps(36);
     overflow : hidden;
     white-space: nowrap;
-    border-right: 2px solid white;
-    width: 35ch;
+    border-right: 2px solid rgba(255,255,255,0.75);
+    width: 36ch;
     letter-spacing: 1.3px;
+    transform: translateY(-50%);
     
 }
-@keyframes type {
+.anim-typewriter{
+  animation: typewriter 3s steps(36) 1s 1 normal both,
+  blinkTextCursor 500ms steps(36) infinite normal;
+}
+@keyframes typewriter {
   0% {
     width: 0ch;
   }
   100% {
-    width: 35ch;
+    width: 36ch;
   }
   
 }
-
+@keyframes blinkTextCursor {
+  from{
+    border-right-color: rgba(255,255,255,0.75);
+  }
+  to{
+    border-right-color: transparent;
+  }
+}
 </style>
