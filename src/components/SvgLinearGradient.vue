8fc84d67ca619ae0654d446d44fcddaead5b2560
0racularGh0st <nigel4tariang@gmail.com>
2020-10-18 16:16:57 +0530

    Add svg linear gradient code

---
+++ src/components/SvgLinearGradient.vue
@@ -0,0 +1,39 @@
+<template>
+     <svg
+              id="hexagon"
+              viewBox="0 0 100 100"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              xmlns:xlink="http://www.w3.org/1999/xlink"
+            >
+              <defs>
+                <linearGradient id="main-gradient" x1="0%" y1="0%" x2="100%" y2="0%">
+                  <stop
+                    offset="0%"
+                    style="stop-color: rgb(62, 110, 173); stop-opacity: 1"
+                  />
+                  <stop
+                    offset="100%"
+                    style="stop-color: rgb(106, 228, 230); stop-opacity: 1"
+                  />
+                </linearGradient>
+              </defs>
+              <path
+                stroke="url(#grad1)"
+                stroke-width="5"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M 50, 5
+         L 11, 27
+         L 11, 72
+         L 50, 95
+         L 89, 73
+         L 89, 28 z"
+              />
+            </svg>
+</template>
+<style scoped>
+.svg-linear-gradient{
+    display: none;
+}
+</style>
--- src/views/MainContent.vue
+++ src/views/MainContent.vue
@@ -4,6 +4,7 @@
     <background ></background>
     <home class="home-class"></home>
     <mynavbar></mynavbar>
+    <social></social>
   </div>
 </div>
 </template>
@@ -12,6 +13,7 @@ import { defineAsyncComponent } from 'vue';
 const Background = defineAsyncComponent(() => import("../components/Background" /* webpackChunkName: "background" */));
 const Home = defineAsyncComponent(() => import("./Home" /* webpackChunkName: "home" */));
 const Navbar = defineAsyncComponent(() => import("../components/Navbar" /* webpackChunkName: "navbar" */));
+const Social = defineAsyncComponent(()=> import("../components/Social" /* webpackChunkName: "social" */));
 export default {
   data(){
     return {
@@ -21,7 +23,8 @@ export default {
   components:{
     'background': Background,
     'home': Home,
-    'mynavbar' : Navbar
+    'mynavbar' : Navbar,
+    'social' : Social
   }
 }
 </script>
