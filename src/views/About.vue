22c1a2cd8517130126fdeea98947fe4bb480af4d
0racularGh0st <nigel4tariang@gmail.com>
2020-10-22 00:21:47 +0530

    Add section stylings with different max-widths for different breakpoints

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="about content-section">
+  <section class="about content-section">
     <div>Normal Div</div>
     <h1>This is H1 </h1>
     <h2>This is H2</h2>
     <h3>This is H3</h3>
     <h4>This is H4</h4>
     <p>This is P</p>
-   </div>
+   </section>
 </template>
 <script>
 import './sections.css';
