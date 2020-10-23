a50b4d661f588c99d8b23398b9385301e2edd7a5
0racularGh0st <nigel4tariang@gmail.com>
2020-10-23 12:07:05 +0530

    use scss instead of css for styled components

--- src/views/About.vue
+++ src/views/About.vue
@@ -5,17 +5,22 @@
         <div class="horizontal-gradient-line"/>
     </div>
     <div class="section-details-container">
-        <p>Hello! I'm Nigel David, a Fullstack Developer from Shillong, Meghalaya - India.</p>
+        <p>Hello! I'm <a href="javascript:;" class="highlight-text">Nigel David</a>, a Fullstack Developer from Shillong, Meghalaya - India.</p>
         <p>I love building things that live on the internet, be it websites, applications or other things in between. 
           My goal is to always build products that focus on Functionality, Performance and Design. 
+        </p>
+        <p>
+          Shortly after graduating from <a href="https://www.iitm.ac.in/" target="_blank" rel="noopener noreferrer" class="highlight-text-link" aria-label="IIT Madras">IIT Madras<span class="hightlight-text-underline"></span></a> (M.Tech CSE), I joined <a href="https://www.telstra.com.au/" target="_blank" rel="noopener noreferrer" class="highlight-text-link" aria-label="Telstra">Telstra<span class="hightlight-text-underline"></span></a> as a Software/DevOps Engineer where I work on various Backend and Frontend applications. 
         </p>
 
     </div>
    </section>
+  
 </template>
+
 <script>
 import './sections.css';
-import './css-styled-components.css';
+import './css-styled-components.scss';
 export default {
   
 }
