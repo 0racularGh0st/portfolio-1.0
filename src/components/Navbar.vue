7edc1a254a90a49198ed5a69be68a5dc2c5b282b
0racularGh0st <nigel4tariang@gmail.com>
2020-10-24 09:34:47 +0530

    Adjust navbar svg and text for different views

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -47,7 +47,7 @@
               <path
                 d="M11 7h2v2h-2zm0 4h2v6h-2zm1-9C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"
               ></path></svg
-            ><span >About Me <span class="hightlight-text-underline"></span></span></a
+            ><span >About Me <span class="hightlight-text-underline-navs"></span></span></a
           >
         </li>
         <li>
@@ -56,7 +56,7 @@
               <path
                 d="M21.49,4.73,17,2.17a1,1,0,0,0-1,0L12,4.45,8,2.17a1,1,0,0,0-1,0L2.51,4.73A1,1,0,0,0,2,5.6v5.12a1,1,0,0,0,.51.87l4,2.27V18.4a1,1,0,0,0,.51.87l4.5,2.56a1,1,0,0,0,1,0L17,19.27a1,1,0,0,0,.51-.87V13.86l4-2.27a1,1,0,0,0,.51-.87V5.6A1,1,0,0,0,21.49,4.73ZM4,10.14v-4l3.5-2,3.5,2v4l-3.5,2Zm11.5,7.68-3.5,2-3.5-2v-4l3.5-2,3.5,2ZM20,10.14l-3.5,2-3.5-2v-4l3.5-2,3.5,2Z"
               ></path></svg
-            ><span >Skills<span class="hightlight-text-underline"></span></span></a
+            ><span >Skills<span class="hightlight-text-underline-navs"></span></span></a
           >
         </li>
          <li>
@@ -65,7 +65,7 @@
               <path
                 d="M22.78,10.37A1,1,0,0,0,22,10H20V9a3,3,0,0,0-3-3H10.72l-.32-1A3,3,0,0,0,7.56,3H4A3,3,0,0,0,1,6V18a3,3,0,0,0,3,3H18.4a3,3,0,0,0,2.92-2.35L23,11.22A1,1,0,0,0,22.78,10.37ZM5.37,18.22a1,1,0,0,1-1,.78H4a1,1,0,0,1-1-1V6A1,1,0,0,1,4,5H7.56a1,1,0,0,1,1,.68l.54,1.64A1,1,0,0,0,10,8h7a1,1,0,0,1,1,1v1H8a1,1,0,0,0-1,.78Zm14,0a1,1,0,0,1-1,.78H7.21a1.42,1.42,0,0,0,.11-.35L8.8,12h12Z"
               ></path></svg
-            ><span >Projects<span class="hightlight-text-underline"></span></span></a
+            ><span >Projects<span class="hightlight-text-underline-navs"></span></span></a
           >
         </li>
       </ul>
@@ -235,9 +235,7 @@ export default {
   width: 45%;
   font-family: 'Work Sans-light';
 }
-.nav-links li:hover{
-  color: rgb(106, 228, 230);
-}
+
 @media (min-width: 769px) {
   .nav-links li {
     list-style: none;
@@ -273,14 +271,12 @@ export default {
   display: flex;
   align-items: center;
 }
-.nav-links a:hover{
-  color: rgb(106, 228, 230);
-}
+
 .nav-links svg {
   height: 23px;
   width: 23px;
   margin-right: 2px;
-  margin-bottom: 1px;
+  margin-bottom: 3px;
   transform: scale(0.65);
 }
 @media (min-width: 320px) {
@@ -294,6 +290,13 @@ export default {
   .hidenav {
     top: -64px;
   }
+  .nav-links svg {
+  height: 23px;
+  width: 23px;
+  margin-right: 2px;
+  margin-bottom: 3px;
+  transform: scale(0.65);
+}
 }
 .mylogo > img {
   height: 30px;
@@ -346,6 +349,13 @@ export default {
     opacity: 0;
     transition: all 0.5s ease;
   }
+  .nav-links svg {
+  height: 23px;
+  width: 23px;
+  margin-right: 2px;
+  margin-bottom: 5px;
+  transform: scale(0.65);
+}
   .burger {
     display: block;
     cursor: pointer;
