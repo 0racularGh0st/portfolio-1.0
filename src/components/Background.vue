e08416e31fd1808b7606dfd22649292429c6a092
0racularGh0st <nigel4tariang@gmail.com>
2020-10-26 22:52:58 +0530

    adjustments to particlejs:

--- src/components/Background.vue
+++ src/components/Background.vue
@@ -1,216 +1,224 @@
 <template>
-  <div id="the-background" class="the-background" v-bind:style="backgroundHeight">
+  <div
+    id="the-background"
+    class="the-background"
+    v-bind:style="backgroundHeight"
+  >
     <div class="starsandtwinkle">
       <div class="still-stars">
-      <div class="twinkling"></div> 
+        <div class="twinkling"></div>
+      </div>
     </div>
-    </div>
     <div class="star"></div>
     <div class="star"></div>
     <div class="star"></div>
     <div class="star"></div>
-     <div class="star"></div>
-     <div id="moon" class= "moon"></div>
-     <Particles
+    <div class="star"></div>
+    <div id="moon" class="moon"></div>
+    <Particles
       id="tsparticles"
       :options="{
-            background: {
-                color: {
-                    value: 'transparent'
-                }
+        background: {
+          color: {
+            value: 'transparent',
+          },
+        },
+        fpsLimit: 40,
+        interactivity: {
+          detectsOn: 'window',
+          events: {
+            onClick: {
+              enable: false,
+              mode: 'push',
             },
-            fpsLimit: 60,
-            interactivity: {
-                detectsOn: 'canvas',
-                events: {
-                    onClick: {
-                        enable: true,
-                        mode: 'push'
-                    },
-                    onHover: {
-                        enable: true,
-                        mode: 'grab'
-                    },
-                    resize: true
-                },
-                modes: {
-                    bubble: {
-                        distance: 200,
-                        duration: 2,
-                        opacity: 0.8,
-                        size: 40
-                    },
-                    push: {
-                        quantity: 4
-                    },
-                    repulse: {
-                        distance: 200,
-                        duration: 0.4
-                    }
-                }
+            onHover: {
+              enable: true,
+              mode: 'grab',
             },
+            resize: true,
+          },
+          modes: {
+            push: {
+              quantity: 2,
+            },
+            grab: {
+              distance: 150,
+            },
+          },
+        },
+        particles: {
+          color: {
+            value: '#ffffff',
+          },
+          links: {
+            color: '#ffffff',
+            distance: 150,
+            enable: true,
+            opacity: 0.5,
+            width: 1,
+          },
+          collisions: {
+            enable: true,
+          },
+          move: {
+            direction: 'none',
+            enable: true,
+            outMode: 'bounce',
+            random: false,
+            speed: 4,
+            straight: false,
+          },
+          number: {
+            density: {
+              enable: false,
+              value_area: 0,
+            },
+            value: 40,
+          },
+          opacity: {
+            value: 0.4,
+          },
+          shape: {
+            type: 'circle',
+          },
+          size: {
+            random: true,
+            value: 4,
+          },
+          twinkle: {
             particles: {
-                color: {
-                    value: '#ffffff'
-                },
-                links: {
-                    color: '#ffffff',
-                    distance: 150,
-                    enable: true,
-                    opacity: 0.5,
-                    width: 1
-                },
-                collisions: {
-                    enable: true
-                },
-                move: {
-                    direction: 'none',
-                    enable: true,
-                    outMode: 'bounce',
-                    random: false,
-                    speed: 6,
-                    straight: false
-                },
-                number: {
-                    density: {
-                        enable: true,
-                        value_area: 800
-                    },
-                    value: 80
-                },
-                opacity: {
-                    value: 0.5
-                },
-                shape: {
-                    type: 'circle'
-                },
-                size: {
-                    random: true,
-                    value: 5
-                }
+              enable: true,
+              frequency: 0.05,
+              opacity: 1,
             },
-            detectRetina: true
-        }"
+          },
+        },
+        detectRetina: false,
+      }"
     />
   </div>
 </template>
 <script>
 export default {
-  data(){
-    return{
-      moon: '',
-      background: '',
-      viewportDiff: ''
-    }
+  data() {
+    return {
+      moon: "",
+      background: "",
+      viewportDiff: "",
+    };
   },
   computed: {
-    backgroundHeight: function(){
+    backgroundHeight: function () {
       return {
-         height: "calc(100vh + "+this.viewportDiff+"px)",
-         top: "0",
-         left: "0",
-         right: "0",
-         bottom: "0",
-         position: "fixed",
-         backgroundSize: "cover",
-         backgroundAttachment: "fixed",
-         background: "inherit"
-      }
-    }
-  }
-  ,
-
-  mounted(){
-     this.viewportDiff = document.documentElement.clientHeight - window.innerHeight;
-    this.moon = document.getElementById('moon');
-    this.moon.style.left = '0px';
-    this.moon.style.top = '0px';
-    window.addEventListener('scroll',function(){
-        let value = window.scrollY;
-        this.moon.style.left= (value/2)+'px';
-        this.moon.style.top=-(value/3)+'px';
+        height: "calc(100vh + " + this.viewportDiff + "px)",
+        top: "0",
+        left: "0",
+        right: "0",
+        bottom: "0",
+        position: "fixed",
+        backgroundSize: "cover",
+        backgroundAttachment: "fixed",
+        background: "inherit",
+      };
+    },
+  },
+  mounted() {
+    this.viewportDiff =
+      document.documentElement.clientHeight - window.innerHeight;
+    this.moon = document.getElementById("moon");
+    this.moon.style.left = "0px";
+    this.moon.style.top = "0px";
+    window.addEventListener("scroll", function () {
+      let value = window.scrollY;
+      this.moon.style.left = value / 2 + "px";
+      this.moon.style.top = -(value / 3) + "px";
     });
-  }
-}
+  },
+};
 </script>
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-
+<style lang="scss" scoped>
 * {
-    margin: 0;
-    padding: 0;
+  margin: 0;
+  padding: 0;
 }
-body{
+body {
   background: black;
 }
 
 @keyframes move-twink-back {
-    from {background-position:0 0;}
-    to {background-position:-10000px 5000px;}
+  from {
+    background-position: 0 0;
+  }
+  to {
+    background-position: -10000px 5000px;
+  }
 }
 
-.moon{
-  position:absolute;
-  top:0;
-  left:0;
-  right:0;
-  bottom:0;
-  width:100%;
-  height:100%;
-  display:block;
+.moon {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  width: 100%;
+  height: 100%;
+  display: block;
 }
-.still-stars, .twinkling, .starsandtwinkle{
-  top:0;
-  left:0;
-  right:0;
-  bottom:0;
-  width:100%;
-  height:100%;
-  display:block;
+.still-stars,
+.twinkling,
+.starsandtwinkle {
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  width: 100%;
+  height: 100%;
+  display: block;
 }
 
 .webp .still-stars {
   background: url(../assets/stars.webp) repeat top center #000;
   background-repeat: repeat;
-  z-index:0;
+  z-index: 0;
 }
 .no-webp .still-stars {
   background: url(../assets/stars.jpg) repeat top center #000;
   background-repeat: repeat;
-  z-index:0;
+  z-index: 0;
 }
-.webp .twinkling{
+.webp .twinkling {
   background: url(../assets/twinkling.webp) repeat top center transparent;
-  z-index:1;
-  animation:move-twink-back 200s linear infinite;
+  z-index: 1;
+  animation: move-twink-back 200s linear infinite;
 }
-.no-webp .twinkling{
-  background:url(../assets/twinkling.jpg) repeat top center transparent;
-  z-index:1;
-  animation:move-twink-back 200s linear infinite;
+.no-webp .twinkling {
+  background: url(../assets/twinkling.jpg) repeat top center transparent;
+  z-index: 1;
+  animation: move-twink-back 200s linear infinite;
 }
 
-.webp .moon{
-    background: url(../assets/rsz_moon.webp) top right transparent;
-    background-repeat: no-repeat;
-    z-index: 6;
-    background-size: 10rem;
-    background-position-y: 30%;
-    background-position-x: 90%;
-    filter: brightness(0.9);
-    left: 0px;
-    transition: all 0s ease;
+.webp .moon {
+  background: url(../assets/rsz_moon.webp) top right transparent;
+  background-repeat: no-repeat;
+  z-index: 6;
+  background-size: 10rem;
+  background-position-y: 30%;
+  background-position-x: 90%;
+  filter: brightness(0.9);
+  left: 0px;
+  transition: all;
 }
-.no-webp .moon{
-    background:url(../assets/rsz_moon.jpg) top right transparent;
-    background-repeat: no-repeat;
-    z-index: 6;
-    background-size: 10rem;
-    background-position-y: 30%;
-    background-position-x: 90%;
-    filter: brightness(0.9);
-    left: 0px;
-    transition: all 0s ease;
+.no-webp .moon {
+  background: url(../assets/rsz_moon.jpg) top right transparent;
+  background-repeat: no-repeat;
+  z-index: 6;
+  background-size: 10rem;
+  background-position-y: 30%;
+  background-position-x: 90%;
+  filter: brightness(0.9);
+  left: 0px;
+  transition: all;
 }
 .star {
   position: absolute;
@@ -219,17 +227,23 @@ body{
   height: 2px;
   background: linear-gradient(-45deg, #cbcbcb, rgba(0, 0, 255, 0));
   filter: drop-shadow(0 0 6px #699bff);
-  animation: tail 3000ms ease-in-out, shooting 3000ms ease-in-out ;
+  animation: tail 3000ms ease-in-out, shooting 3000ms ease-in-out;
 }
 
-.star::before, .star::after {
+.star::before,
+.star::after {
   z-index: 4;
   position: absolute;
-  content: '';
+  content: "";
   top: calc(50% - 1px);
   right: 0;
   height: 2px;
-  background: linear-gradient(-45deg, rgba(0, 0, 255, 0), #cbcbcb, rgba(0, 0, 255, 0));
+  background: linear-gradient(
+    -45deg,
+    rgba(0, 0, 255, 0),
+    #cbcbcb,
+    rgba(0, 0, 255, 0)
+  );
   border-radius: 100%;
   transform: translateX(50%) rotateZ(-45deg);
   animation: shining 3000ms ease-in-out;
@@ -240,40 +254,43 @@ body{
 }
 
 .star:nth-child(5) {
-   z-index: 4;
-   opacity: 0.5;
+  z-index: 4;
+  opacity: 0.5;
   top: calc(31%);
   left: calc(86% - 300px);
   animation-delay: 2s;
   animation-iteration-count: 1;
 }
-.star:nth-child(5)::before, .star:nth-child(5)::after {
+.star:nth-child(5)::before,
+.star:nth-child(5)::after {
   animation-delay: 2s;
   animation-iteration-count: 1;
 }
 
 .star:nth-child(6) {
-   z-index: 4;
-   opacity: 0.5;
+  z-index: 4;
+  opacity: 0.5;
   top: calc(41% - -50px);
   left: calc(50% - 190px);
   animation-delay: 5s;
   animation-iteration-count: 1;
 }
-.star:nth-child(6)::before, .star:nth-child(6)::after {
+.star:nth-child(6)::before,
+.star:nth-child(6)::after {
   animation-delay: 5s;
   animation-iteration-count: 1;
 }
 
 .star:nth-child(7) {
-   z-index: 4;
-   opacity: 0.5;
+  z-index: 4;
+  opacity: 0.5;
   top: calc(34%);
   left: calc(71%);
   animation-delay: 9s;
   animation-iteration-count: 1;
 }
-.star:nth-child(7)::before, .star:nth-child(7)::after {
+.star:nth-child(7)::before,
+.star:nth-child(7)::after {
   animation-delay: 9s;
   animation-iteration-count: 1;
 }
@@ -286,37 +303,36 @@ body{
   animation-delay: 13s;
   animation-iteration-count: 1;
 }
-.star:nth-child(4)::before, .star:nth-child(4)::after {
+.star:nth-child(4)::before,
+.star:nth-child(4)::after {
   animation-delay: 13s;
   animation-iteration-count: 1;
 }
 
 .star:nth-child(8) {
-   z-index: 4;
-   opacity: 0.5;
+  z-index: 4;
+  opacity: 0.5;
   top: calc(-5% - -190px);
   left: calc(74% - 200px);
   animation-delay: 17s;
   animation-iteration-count: 1;
 }
-.star:nth-child(8)::before, .star:nth-child(8)::after {
+.star:nth-child(8)::before,
+.star:nth-child(8)::after {
   animation-delay: 17s;
   animation-iteration-count: 1;
 }
-
-
-
 
 /* Animations */
 @keyframes tail {
   0% {
     width: 0;
   }
-  
+
   30% {
     width: 80px;
   }
-  
+
   100% {
     width: 0;
   }
@@ -342,15 +358,16 @@ body{
     transform: translateX(320px) rotate(-15deg) translateY(-65px);
   }
 }
-#tsparticles{
-  top:0;
-  left:0;
-  right:0;
-  bottom:0;
-  width:100%;
-  height:100%;
-  display:block;
+#tsparticles {
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  width: 100vw;
+  height: 100vh;
+  display: block;
   position: absolute;
   z-index: 5;
+  opacity: 0.7;
 }
 </style>
