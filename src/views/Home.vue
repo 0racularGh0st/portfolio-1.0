8035a2cb5bfd6f8b912e9822349270606648a84d
0racularGh0st <nigel4tariang@gmail.com>
2020-10-28 00:10:07 +0530

    Add sub intro text

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,147 +1,158 @@
 <template>
   <div>
     <div class="home">
-    <section>
-      <h1 class="content">
-      <div id="line-typing" class="line-typing">
-        
-      </div>
-      
-    </h1>
-    </section>
-  </div>
-  <about/>
-  <skills/>
+      <section>
+        <h1 class="content">
+          <div id="line-typing" class="line-typing"></div>
+        </h1>
+        <h2 class="content-2">I build things that live on the internet.</h2>
+      </section>
+    </div>
+    <about />
+    <skills />
+    <projects />
   </div>
 </template>
 
 <script>
-window.addEventListener("load", function() { 
-  
-  setTimeout(startType,3000);
+window.addEventListener("load", function () {
+  setTimeout(startType, 3000);
 });
-let index=0;
-const mainText= "Hi, I'm Nigel. ";
-const texts = ["FullStack Developer.","DevOps Engineer."]
-let letter='';
+let index = 0;
+const mainText = "Hi, I'm Nigel. ";
+const texts = ["FullStack Developer.", "DevOps Engineer."];
+let letter = "";
 let mainTextDone = false;
 let count = 0;
 let currentText = "";
-let direction="forward";
-let prevDirection="forward";
-const startType = ()=>{
-          if(document.querySelector('.line-typing')){
-            if(index<=mainText.length && !mainTextDone){
-              letter = mainText.slice(0,++index);
-              document.querySelector('.line-typing').textContent = letter;
-                if(index > mainText.length)
-                {
-                  mainTextDone = true;
-                  index = 0;
-                }
-            }
-            else{
-               if(count === texts.length)
-               {
-                 count = 0;
-               }
+let direction = "forward";
+let prevDirection = "forward";
+const startType = () => {
+  if (document.querySelector(".line-typing")) {
+    if (index <= mainText.length && !mainTextDone) {
+      letter = mainText.slice(0, ++index);
+      document.querySelector(".line-typing").textContent = letter;
+      if (index > mainText.length) {
+        mainTextDone = true;
+        document.querySelector(".content-2").classList.add("content-2-show");
+        index = 0;
+      }
+    } else {
+      if (count === texts.length) {
+        count = 0;
+      }
 
-               currentText = texts[count];
-               if(direction === "forward"){
-                 letter = currentText.slice(0,++index);
-               document.querySelector('.line-typing').textContent = mainText+letter;
-               if(letter.length === currentText.length)
-               {
-                 direction= "backward"
-                //  count++;
-                //  index = 0;
-               }
-               }
-               if(direction === "backward"){
-                 letter = currentText.slice(0,index--);
-               document.querySelector('.line-typing').textContent = mainText+letter;
-               if(letter.length === 0)
-               {
-                 direction= "forward"
-                 count++;
-                 index = 0;
-               }
-               }
-
-            }
-          }
-          // if(index!==text.length)
-          if(prevDirection === direction)
-          {setTimeout(startType,70);}
-          else{
-            prevDirection = direction;
-            if(prevDirection === "forward")
-            setTimeout(startType,500);
-            else{
-              setTimeout(startType,2000);
-            }
-          }
-}
-import { defineAsyncComponent } from 'vue';
-const About = defineAsyncComponent(() => import("./About" /* webpackChunkName: "about" */));
-const Skills = defineAsyncComponent(() => import("./Skills" /* webpackChunkName: "skills" */));
+      currentText = texts[count];
+      if (direction === "forward") {
+        letter = currentText.slice(0, ++index);
+        document.querySelector(".line-typing").textContent = mainText + letter;
+        if (letter.length === currentText.length) {
+          direction = "backward";
+          //  count++;
+          //  index = 0;
+        }
+      }
+      if (direction === "backward") {
+        letter = currentText.slice(0, index--);
+        document.querySelector(".line-typing").textContent = mainText + letter;
+        if (letter.length === 0) {
+          direction = "forward";
+          count++;
+          index = 0;
+        }
+      }
+    }
+  }
+  // if(index!==text.length)
+  if (prevDirection === direction) {
+    setTimeout(startType, 70);
+  } else {
+    prevDirection = direction;
+    if (prevDirection === "forward") setTimeout(startType, 500);
+    else {
+      setTimeout(startType, 2000);
+    }
+  }
+};
+import { defineAsyncComponent } from "vue";
+const About = defineAsyncComponent(() =>
+  import("./About" /* webpackChunkName: "about" */)
+);
+const Skills = defineAsyncComponent(() =>
+  import("./Skills" /* webpackChunkName: "skills" */)
+);
+const Projects = defineAsyncComponent(() =>
+  import("./Projects" /* webpackChunkName: "projects" */)
+);
 
 export default {
-    data(){
-      return {
-        text: "Hi, I'm Nigel. FullStack Developer.",
-        index : 0,
-        letter : ''
-      }
-    },
-   components:{
-     'about':About,
-     'skills': Skills
-   }
-   
-}
+  data() {
+    return {
+      text: "Hi, I'm Nigel. FullStack Developer.",
+      index: 0,
+      letter: "",
+    };
+  },
+  components: {
+    about: About,
+    skills: Skills,
+    projects: Projects,
+  },
+};
 </script>
 <style scoped>
-.home{
+.home {
   height: 50vh;
 }
-.content{
-  margin-top: 50vh;
+.content {
+  margin: 50vh auto 0 auto;
   width: 31ch;
-  margin-left: auto;
-  margin-right: auto;
 }
-.line-typing{
-    color:#cee8f5;
-    overflow : hidden;
-    white-space: nowrap;
-    transform: translateY(-50%);
+.line-typing {
+  color: #cee8f5;
+  overflow: hidden;
+  white-space: nowrap;
+  transform: translateY(-50%);
 }
-   
-.line-typing::after{
-  content: '';
-  border-right: 2px solid rgba(255,255,255,0.75);
+
+.line-typing::after {
+  content: "";
+  border-right: 2px solid rgba(255, 255, 255, 0.75);
   animation: blinkTextCursor 500ms infinite;
   animation-delay: 2s;
   animation-direction: normal;
   animation-fill-mode: both;
 }
 
-
 @keyframes blinkTextCursor {
-  0%{
-    border-right-color: rgba(255,255,255,0.75);
+  0% {
+    border-right-color: rgba(255, 255, 255, 0.75);
   }
-  100%{
+  100% {
     border-right-color: transparent;
   }
 }
 @-webkit-keyframes blinkTextCursor {
-  0%{
-    border-right-color: rgba(255,255,255,0.75);
+  0% {
+    border-right-color: rgba(255, 255, 255, 0.75);
   }
-  100%{
+  100% {
     border-right-color: transparent;
   }
+}
+.content-2 {
+  color: #cee8f5;
+  margin-top: unset;
+  max-width: 100vw;
+  width: max-content;
+  margin-left: auto;
+  margin-right: auto;
+  opacity: 0;
+  transform: translateY(3rem);
+  transition: opacity 0.5s cubic-bezier(0.25, 0.46, 0.45, 0.94) .2s, transform 0.5s cubic-bezier(0.25, 0.46, 0.45, 0.94) .2s;
+}
+.content-2-show{
+  opacity: 0.7;
+  transform: translateY(0);
 }
 </style>
---
+++ src/views/Projects.vue
@@ -0,0 +1,57 @@
+<template>
+  <section id="projects" class="projects-main-section">
+    <div class="section-name">
+      <h1 class="section-name-text-left">Projects</h1>
+      <div class="horizontal-gradient-line-to-right" />
+    </div>
+    <div class="section-details-container">
+      <p>To be added soon! Check back later. </p>
+    </div>
+  </section>
+</template>
+<script>
+import './sections.scss';
+import './css-styled-components.scss';
+export default {
+    mounted(){
+    this.observeMethod();
+  },
+  methods: {
+    observeMethod: function(){
+      let options = {
+  root: document.querySelector('main-app'),
+  rootMargin: '0px',
+  threshold: 0.1
+}
+ const subSection = document.querySelector(".projects-main-section");
+        let observer = new IntersectionObserver((entry) => {
+            if(entry[0].isIntersecting)
+            {
+              subSection.classList.add("sub-section-show");
+               observer.disconnect();
+            }
+        },options);
+        observer.observe(subSection);
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+.projects-main-section{
+  color : #cee8f5;
+  margin-top: 5rem;
+  margin-bottom: 5rem;
+  transition: opacity 0.5s cubic-bezier(0.25, 0.46, 0.45, 0.94) 0.2s, transform 0.5s cubic-bezier(0.25, 0.46, 0.45, 0.94) 0.2s;
+  opacity: 0;
+  transform: translateY(3rem);
+  -webkit-transform: translateY(3rem);
+}
+.sub-section-show{
+  opacity: 1;
+  transform: translateY(0rem);
+  -webkit-transform: translateY(0rem);
+}
+.section-details-container{
+  color: #bec2d2;
+}
+</style>
