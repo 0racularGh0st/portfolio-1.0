71c9ee2050c5892807c629cdf7f6821c513c61b7
0racularGh0st <nigel4tariang@gmail.com>
2020-12-11 10:17:12 +0530

    add service status project

--- src/config/index.js
+++ src/config/index.js
@@ -13,6 +13,13 @@ module.exports = {
             website: "https://peer-vue.web.app/",
             description: "A Progressive Web App for Audio/Video Chatting along with a Text messaging chat window. This project involves 4 integral parts, the Signalling Server, the Peer Server, the Client and the TURN server which is used to relay audio, video, and data streaming between peers if the peers are behind firewalls or proxies.",
             tags: ["VueJS","NodeJS","Express","PeerJs","Socket.io","Heroku","Firebase"]
+        },
+        serviceStatus: {
+            name: "Service Status Checker",
+            github: "https://github.com/0racularGh0st/service-status-checker",
+            website: "https://service-status-nigeldavid.web.app/",
+            description: "A Progressive Web App for monitoring the health status of all my web apps and servers in one common space. The server for this project also runs a scheduled Cron Job everyday at 9:00 am IST and sends me a notification through Web-Push if any service is down. The server runs in my personal OVH Cloud VPS.",
+            tags: ["ReactJS","Web-Push","Cron","NodeJS","Express","OVH Cloud","Firebase"]
         }
     }
 }
