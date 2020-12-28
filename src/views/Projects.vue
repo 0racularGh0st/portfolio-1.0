fcb226f7bb146177f17bd367ad0fad4890471bf6
0racularGh0st <nigel4tariang@gmail.com>
2020-12-28 22:45:58 +0530

    add transition to project items idividually

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -5,10 +5,10 @@
       <div class="horizontal-gradient-line-to-right" />
     </div>
     <div class="section-details-container">
-      <Project :imgSrc="webpSupported? 'assets/my-spotify.webp' : 'assets/my-spotify.png'" :config="config.mySpotify" place="left"/>
-      <Project :imgSrc="webpSupported? 'assets/peer-vue.webp' : 'assets/peer-vue.png'" :config="config.peerVue" place="right"/>
-      <Project :imgSrc="webpSupported? 'assets/service-status.webp' : 'assets/service-status.png'" :config="config.serviceStatus" place="left"/>
-      <Project :imgSrc="webpSupported? 'assets/craftastic.webp' : 'assets/craftastic.png'" :config="config.craftastic" place="right"/>
+      <Project :imgSrc="webpSupported? 'assets/my-spotify.webp' : 'assets/my-spotify.png'" :config="config.mySpotify" place="left" class="project-item"/>
+      <Project :imgSrc="webpSupported? 'assets/peer-vue.webp' : 'assets/peer-vue.png'" :config="config.peerVue" place="right" class="project-item"/>
+      <Project :imgSrc="webpSupported? 'assets/service-status.webp' : 'assets/service-status.png'" :config="config.serviceStatus" place="left" class="project-item"/>
+      <Project :imgSrc="webpSupported? 'assets/craftastic.webp' : 'assets/craftastic.png'" :config="config.craftastic" place="right" class="project-item"/>
     </div>
   </section>
 </template>
@@ -31,7 +31,7 @@ export default {
     mounted(){
       console.log("Mounted");
       this.observeMethod();
-  },
+    },
   data(){
     return{
       webpSupported: false,
@@ -41,20 +41,34 @@ export default {
   methods: {
     observeMethod: function(){
       let options = {
-  root: document.querySelector('main-app'),
-  rootMargin: '0px',
-  threshold: 0.1
-}
- const subSection = document.querySelector(".projects-main-section");
-        let observer = new IntersectionObserver((entry) => {
-            if(entry[0].isIntersecting)
-            {
-              subSection.classList.add("sub-section-show");
-               observer.disconnect();
-            }
-        },options);
-        observer.observe(subSection);
-    }
+      root: document.querySelector('main-app'),
+      rootMargin: '0px',
+      threshold: 0.1
+        }
+        const subSection = document.querySelector(".projects-main-section");
+              let observer = new IntersectionObserver((entry) => {
+                  if(entry[0].isIntersecting)
+                  {
+                    subSection.classList.add("sub-section-show");
+                    observer.disconnect();
+                  }
+              },options);
+              observer.observe(subSection);
+        const projectItems = document.querySelectorAll(".project-item");
+              let itemObserver = new IntersectionObserver((entries) => {
+                    entries.forEach(entry => {
+                      if(entry.isIntersecting)
+                      {
+                        entry.target.classList.add("show");
+                        itemObserver.unobserve(entry.target);
+                      }
+                    })
+              },options);
+              projectItems.forEach((projectItem)=>{
+                itemObserver.observe(projectItem);
+              })
+
+      }
   },
   components: {
     Project
@@ -70,6 +84,17 @@ export default {
   opacity: 0;
   transform: translateY(3rem);
   -webkit-transform: translateY(3rem);
+}
+.project-item{
+  opacity: 0;
+  transform: translateY(3rem);
+  -webkit-transform: translateY(3rem);
+  transition: opacity 0.5s cubic-bezier(0.25, 0.46, 0.45, 0.94) 0.2s, transform 0.5s cubic-bezier(0.25, 0.46, 0.45, 0.94) 0.2s;
+  &.show{
+    opacity: 1;
+    transform: translateY(0rem);
+    -webkit-transform: translateY(0rem);
+  }
 }
 .sub-section-show{
   opacity: 1;
